05e3e8ff44478d02e7a1a50f93d555fcae9cc8d7
Botdeveloper358 <indonesiyaserver852101@gmail.com>
2025-07-17 20:30:05 +0530

    payment.jsx

---
+++ pages/pages/payment.jsx
@@ -0,0 +1,7 @@
+export default function Payment() {
+    return (
+        <div className="h-screen flex items-center justify-center bg-gray-950 text-white">
+            <h1 className="text-4xl">💳 Payment Page</h1>
+        </div>
+    );
+}

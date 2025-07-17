7354d51ffd52bac9c9531bd8224103d32cb0d577
Botdeveloper358 <indonesiyaserver852101@gmail.com>
2025-07-17 20:28:53 +0530

    dashboard.jsx

---
+++ pages/dashboard.jsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+        <div className="h-screen flex items-center justify-center bg-black text-white">
+            <h1 className="text-4xl">🚀 Welcome to Dashboard</h1>
+        </div>
+    );
+}
